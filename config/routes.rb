9c1da6d47a6e231d80fbaf5c701749d33ee61504
liviu <unknownliviu@gmail.com>
2013-03-31 19:36:17 +0300

    finished layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact' 
+  match '/signup'		=> 'users#new'
+  match '/help'			=> 'static_pages#help'
+  match '/about'   	=> 'static_pages#about'
+  match '/contact'	=> 'static_pages#contact' 
 end
