b248625b085d4561629f0a1c6fa635f1cde11341
liviu <unknownliviu@gmail.com>
2013-04-06 01:37:16 +0300

    f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end  
+
+  def create
+  	@user= User.new(params[:user])
+  	if @user.save
+  		redirect_to @user
+  		flash[:success] = "Welcome to the Sample App!"
+  	else
+  		render 'new'
+  	end
+  end
 end
