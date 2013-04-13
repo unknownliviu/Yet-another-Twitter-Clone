532c03d3c14bd49a23102f81615a513c5ed1d3c3
liviu <unknownliviu@gmail.com>
2013-04-13 14:56:55 +0300

    almost there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  #force signout to prevent CSRF
+  def handle_unverified_request
+  	sign_out
+  	super
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+
+	def sign_in(user)
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+	#is the same as
+	#cookies[:remember_token] = { value:   user.remember_token,
+  #                           expires: 20.years.from_now.utc }
+
+  def current_user=(user)
+  	@current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def signed_in?
+  	!current_user.nil?
+  end
 end
