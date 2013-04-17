40019616837b060f2ace9993a50988a79cae4f48
liviu <unknownliviu@gmail.com>
2013-04-17 22:06:43 +0300

    optimisations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only:  :destroy
+  before_filter :no_create_after,only: [:new, :create]
 
   def new
   	@user = User.new
@@ -40,7 +41,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    user = User.find(params[:id])
+    user.destroy unless current_user?(user)
     flash[:success] = "User killed!"
     redirect_to users_url
   end
@@ -50,16 +52,22 @@ private
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to signin_url, notice: "Please sign in." 
+      redirect_to signin_url, notice: "Yeah... can't go there unless you sign in." 
+    end
+  end
+
+  def no_create_after
+    if signed_in?
+      redirect_to root_path, notice: "Nope, can't do that while signed in"
     end
   end
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to( root_path ) unless current_user?(@user)
+    redirect_to root_path, notice: "Trying to hack others, are we?" unless current_user?(@user)
   end  
 
   def admin_user
-    redirect_to root_path unless current_user.admin?
+    redirect_to root_path, notice: "Chuck Norris will hear about this!" unless current_user.admin?
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -73,7 +73,7 @@ describe "signup" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       describe "after saving the user" do
