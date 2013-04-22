168a822e62f4d7c8754256ec144b10520f63f75c
liviu <unknownliviu@gmail.com>
2013-04-22 12:37:48 +0300

    added validations

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -23,5 +23,15 @@ describe Relationship do
     it { should respond_to(:followed) }
     its(:follower) { should == follower }
     its(:followed) { should == followed }
-  end  
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end    
 end
